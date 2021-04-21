1b7f39608bd5fc35d214d9f91c8e3f71ef093ea2
José Willamy Cunha Braga <willamybraga@yahoo.com>
2021-04-21 19:18:11 -0300

    conversor de moeda em vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-center">Conversor de Moedas</h1>
+    <div class="linha">
+      <Conversor moeda-a="USD" moeda-b="BRL" />
+      <Conversor moeda-a="BRL" moeda-b="USD" />
+    </div>
+       <div class="linha">
+      <Conversor moeda-a="CAD" moeda-b="BRL" />
+      <Conversor moeda-a="BRL" moeda-b="CAD" />
+    </div>
+       <div class="linha">
+      <Conversor moeda-a="EUR" moeda-b="BRL" />
+      <Conversor moeda-a="BRL" moeda-b="EUR" />
+    </div>
+        
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Conversor from './components/Conversor';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Conversor
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  #App{
+    font-family: "Avanir", Arial, Helvetica, sans-serif;
+    color: #2c3e60;
+    margin-top: 60px;
+    max-width: 900px;
+    margin: auto;
+  }
+  .linha{
+    margin-top: 40px;
+    display: flex;
+    margin: auto;
+    justify-items: center;
+    align-items: center;
+    justify-content: center;
+    margin: 10px 10px 10px 10px;
+  }
+  .text-center{
+    text-align: center;
+  }
+  body{
+   background: linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c);
+
+  }
+  </style>
